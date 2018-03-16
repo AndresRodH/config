bad4548c978248d4b83d53f242c77fc57b93fdec
Andres Rodriguez <arodriguez@coolfiresolutions.com>
2018-03-16 15:15:06 -0500

    zshrc config uses oh-my-zsh

--- install/link.sh
+++ install/link.sh
@@ -15,24 +15,6 @@ for file in $linkables ; do
   fi
 done
 
-echo -e "\\n\\nInstalling to ~/.config"
-echo "=============================="
-if [ ! -d "$HOME/.config" ]; then
-  echo "Creating ~/.config"
-  mkdir -p "$HOME/.config"
-fi
-
-config_files=$( find "$DOTFILES/config" -d 1 2>/dev/null )
-for config in $config_files; do
-  target="$HOME/.config/$( basename "$config" )"
-  if [ -e "$target" ]; then
-    echo "~${target#$HOME} already exists... Skipping."
-  else
-    echo "Creating symlink for $config"
-    ln -s "$config" "$target"
-  fi
-done
-
 # create neovim symlinks
 echo -e "\\n\\nCreating neovim symlinks"
 echo "=============================="
