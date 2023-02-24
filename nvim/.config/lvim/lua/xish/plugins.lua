67014e63e4ec44cfde9f4a0ed6c873039ab74e28
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2023-02-24 17:44:40 -0600

    nvim-ts-autotag

--- nvim/.config/lvim/lua/xish/plugins.lua
+++ nvim/.config/lvim/lua/xish/plugins.lua
@@ -8,12 +8,15 @@ M.config = function()
     },
     {
       "kylechui/nvim-surround",
-      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
       config = function()
-        require("nvim-surround").setup({
-          -- Configuration here, or leave empty to use defaults
-        })
+        require("nvim-surround").setup()
       end
+    },
+    {
+      "windwp/nvim-ts-autotag",
+      config = function()
+        require("nvim-ts-autotag").setup()
+      end,
     }
   }
 end
