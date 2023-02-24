6ca67f014c19b0323e9d95d37e9ba92f40da20c4
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2023-02-24 17:34:56 -0600

    nvim-surround

--- nvim/.config/lvim/lua/xish/plugins.lua
+++ nvim/.config/lvim/lua/xish/plugins.lua
@@ -6,6 +6,15 @@ M.config = function()
       "mxsdev/nvim-dap-vscode-js",
       requires = { "mfussenegger/nvim-dap" },
     },
+    {
+      "kylechui/nvim-surround",
+      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+      config = function()
+        require("nvim-surround").setup({
+          -- Configuration here, or leave empty to use defaults
+        })
+      end
+    }
   }
 end
 
