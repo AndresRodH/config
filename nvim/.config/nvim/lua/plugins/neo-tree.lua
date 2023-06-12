57de99d7d64ba4b57e9ca0cfb345e4dcb4ec750c
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2023-06-12 12:44:36 -0500

    remove filtered views override

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -4,16 +4,5 @@ return {
     window = {
       position = "right",
     },
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_by_name = {
-          ".git",
-        },
-      },
-    },
   },
 }
