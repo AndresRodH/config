335b8a7363bd8e7a41f263c53948e5b86c76f3e9
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2023-11-27 11:01:09 -0600

    reenable vitest

--- nvim/.config/nvim/lua/plugins/test.lua
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -13,7 +13,7 @@ return {
     },
     opts = function(_, opts)
       table.insert(opts.adapters, require("neotest-jest"))
-      -- table.insert(opts.adapters, require("neotest-vitest"))
+      table.insert(opts.adapters, require("neotest-vitest"))
     end,
   },
 }
