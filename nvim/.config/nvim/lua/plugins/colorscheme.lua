1728a3d901b569a004fa59bba328ed76d2f7ed03
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2023-11-27 11:01:40 -0600

    colorschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "dracula/vim" },
-
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula"
-    }
-  }
+      colorscheme = "dracula",
+    },
+  },
 }
