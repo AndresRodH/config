675be512a1fe144fc46db914979bac4c51b55664
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2023-11-28 11:35:02 -0600

    chore: set up biome as the default formatter

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      ["javascript"] = { "biome" },
+      ["javascriptreact"] = { "biome" },
+      ["typescript"] = { "biome" },
+      ["typescriptreact"] = { "biome" },
+      ["css"] = { "biome" },
+      ["html"] = { "biome" },
+      ["json"] = { "biome" },
+      ["jsonc"] = { "biome" },
+      ["yaml"] = { "biome" },
+      ["markdown"] = { "biome" },
+      ["markdown.mdx"] = { "biome" },
+      ["graphql"] = { "biome" },
+    },
+  },
+}
