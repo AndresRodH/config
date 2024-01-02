c71285b0a6844a66ad0708ea9652f3ce884685a2
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-02 09:43:58 -0600

    remove type hint

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    ---@type lspconfig.options
     servers = {
       graphql = {
         filetypes = { "graphql", "javascript", "javascriptreact", "typescript", "typescriptreact" },
