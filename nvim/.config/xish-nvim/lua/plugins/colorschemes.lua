d3a9d15a4d5ad9d8277bca5fbae01a515e1ea177
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-09 15:13:24 -0600

    modularize plugins

---
+++ nvim/.config/xish-nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end
+	},
+	{ "rebelot/kanagawa.nvim", name = "kanagawa" },
+}
