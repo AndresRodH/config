4b4883d0482ce65bf427da88a49bf59030c2e960
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-09 15:45:04 -0600

    add some keymaps to lsp

--- nvim/.config/xish-nvim/lua/plugins/lsp.lua
+++ nvim/.config/xish-nvim/lua/plugins/lsp.lua
@@ -1,15 +1,40 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
+	{
 		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
+		cmd = "Mason",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup({})
+		end
 	},
-	config = function()
-		require("mason").setup({})
-		local mason_lspconfig = require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls" }
-		})
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({})
-	end
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" }
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+			settings = {
+					Lua = {
+						diagnostics = {
+							globals = { 'vim' }
+						}
+					}
+				}
+			})
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+		end
+	}
 }
