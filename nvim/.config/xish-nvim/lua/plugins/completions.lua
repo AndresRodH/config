5bd7231eb2ac5bb89b01ba23e82eb12f00c990f2
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-10 15:35:53 -0600

    add path completions to cmp

--- nvim/.config/xish-nvim/lua/plugins/completions.lua
+++ nvim/.config/xish-nvim/lua/plugins/completions.lua
@@ -19,10 +19,11 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
 		dependencies = {
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
 			"saadparwaiz1/cmp_luasnip",
 			{
 				"zbirenbaum/copilot-cmp",
