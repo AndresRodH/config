c8c99b975a0b3453518084a24a496f3d11cf035c
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-10 09:34:49 -0600

    add telescope fzf

--- nvim/.config/xish-nvim/lua/plugins/telescope.lua
+++ nvim/.config/xish-nvim/lua/plugins/telescope.lua
@@ -1,29 +1,35 @@
-return 	{
+return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-		end
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
 		config = function()
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown({})
-					}
-				}
+						require("telescope.themes").get_dropdown({}),
+					},
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
 			})
 			require("telescope").load_extension("ui-select")
-		end
-	}
-}
+			require("telescope").load_extension("fzf")
 
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help pages" })
+		end,
+	},
+}
