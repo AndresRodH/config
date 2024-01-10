42c91392a3e33daf5e80b9f954b0dba3b97718a5
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-10 09:43:54 -0600

    fixes an error triggered from nvim-cmp when insterting tabs in empty lines

--- nvim/.config/xish-nvim/lua/plugins/completions.lua
+++ nvim/.config/xish-nvim/lua/plugins/completions.lua
@@ -4,6 +4,9 @@ return {
 		dependencies = {
 			"rafamadriz/friendly-snippets",
 		},
+		keys = function()
+			return {}
+		end,
 	},
 	{
 		"hrsh7th/nvim-cmp",
@@ -17,6 +20,12 @@ return {
 			local luasnip = require("luasnip")
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
 			cmp.setup({
 				snippet = {
@@ -39,12 +48,13 @@ return {
 							cmp.select_next_item()
 						elseif luasnip.expand_or_jumpable() then
 							luasnip.expand_or_jump()
-						elseif luasnip.has_words_before() then
+						elseif has_words_before() then
 							cmp.complete()
 						else
 							fallback()
 						end
 					end, { "i", "s" }),
+
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
