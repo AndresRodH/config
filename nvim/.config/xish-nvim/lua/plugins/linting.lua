a3e41a591c3c310e85a9a885b10ed6acdc9dc631
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-10 08:17:30 -0600

    add linting with nvim-lint

---
+++ nvim/.config/xish-nvim/lua/plugins/linting.lua
@@ -0,0 +1,33 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			javascript = {
+				"eslint_d",
+			},
+			javascriptreact = {
+				"eslint_d",
+			},
+			typescript = {
+				"eslint_d",
+			},
+			typescriptreact = {
+				"eslint_d",
+			},
+		}
+
+		local lint_group = vim.api.nvim_create_augroup("nvim-lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_group,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
