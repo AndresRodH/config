a2aacf04ea144cf0a85ccf977b60805bcbf755e4
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-09 21:13:47 -0600

    tsserver setup

--- nvim/.config/xish-nvim/lua/plugins/lsp.lua
+++ nvim/.config/xish-nvim/lua/plugins/lsp.lua
@@ -5,15 +5,15 @@ return {
 		build = ":MasonUpdate",
 		config = function()
 			require("mason").setup({})
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" }
+				ensure_installed = { "lua_ls", "tsserver" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -23,18 +23,19 @@ return {
 		},
 		config = function()
 			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({})
 			lspconfig.lua_ls.setup({
-			settings = {
+				settings = {
 					Lua = {
 						diagnostics = {
-							globals = { 'vim' }
-						}
-					}
-				}
+							globals = { "vim" },
+						},
+					},
+				},
 			})
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-		end
-	}
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
