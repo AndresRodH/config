bde1f28daf1f6da3986bb9e00f187988958ce197
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-10 15:43:33 -0600

    put bufferline in ui

--- nvim/.config/xish-nvim/lua/plugins/ui.lua
+++ nvim/.config/xish-nvim/lua/plugins/ui.lua
@@ -20,4 +20,33 @@ return {
 			vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle right<cr>")
 		end,
 	},
+	-- bufffer "tabs"
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+		},
+		opts = {
+			options = {
+				-- use mini.bufremove to close buffers
+				close_command = function(n)
+					require("mini.bufremove").delete(n, false)
+				end,
+				right_mouse_command = function(n)
+					require("mini.bufremove").delete(n, false)
+				end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+			},
+		},
+	},
 }
