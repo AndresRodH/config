e2ad43fd6a096b52de5a27136586ea369aaa6d2a
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-11 10:09:03 -0600

    fix copilot completions not displaying

--- nvim/.config/xish-nvim/lua/plugins/completions.lua
+++ nvim/.config/xish-nvim/lua/plugins/completions.lua
@@ -28,6 +28,9 @@ return {
 			{
 				"zbirenbaum/copilot-cmp",
 				dependencies = "copilot.lua",
+				config = function()
+					require("copilot_cmp").setup()
+				end,
 			},
 		},
 		config = function()
