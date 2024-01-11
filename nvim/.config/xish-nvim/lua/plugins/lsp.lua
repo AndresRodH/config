be222fe1e97fde32f340153f15d7b10c75b37b56
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-11 10:49:17 -0600

    use mason-lspconfig to automatically set up installed servers

--- nvim/.config/xish-nvim/lua/plugins/lsp.lua
+++ nvim/.config/xish-nvim/lua/plugins/lsp.lua
@@ -1,38 +1,40 @@
 return {
 	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		build = ":MasonUpdate",
-		config = function()
-			require("mason").setup({})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
-	},
-	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			"williamboman/mason.nvim",
+			{
+				"williamboman/mason.nvim",
+				cmd = "Mason",
+				build = ":MasonUpdate",
+				config = function()
+					require("mason").setup({})
+				end,
+			},
 			"williamboman/mason-lspconfig.nvim",
 		},
 		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.tsserver.setup({})
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver" },
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+					["lua_ls"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.lua_ls.setup({
+							settings = {
+								Lua = {
+									diagnostics = {
+										globals = { "vim" },
+									},
+								},
+							},
+						})
+					end,
 				},
 			})
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
 			vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, { desc = "Signature Help" })
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
