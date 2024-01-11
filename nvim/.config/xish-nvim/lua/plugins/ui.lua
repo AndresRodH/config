3b3c45e08781ffe8f9d8d86d8b35c31ab6e352b1
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-10 19:49:30 -0600

    lualine

--- nvim/.config/xish-nvim/lua/plugins/ui.lua
+++ nvim/.config/xish-nvim/lua/plugins/ui.lua
@@ -49,4 +49,15 @@ return {
 			},
 		},
 	},
+	-- statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			theme = "dracula",
+		},
+	},
 }
