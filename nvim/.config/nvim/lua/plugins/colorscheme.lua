62f57a77421b9910adde1342961a724e2a59a2d9
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-11 21:10:27 -0600

    set catppuccin as the default colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  { "dracula/vim" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  { "rebelot/kanagawa.nvim", name = "kanagawa" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "dracula",
-    },
-  },
+	{ "dracula/vim" },
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "rebelot/kanagawa.nvim", name = "kanagawa" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
