a68bb2e003c81f349700c6299dbd38a21bd78d72
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-01-11 21:18:26 -0600

    change dashboard logo, add borders to lsp docs, hide close buffer icons

---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		"folke/noice.nvim",
+		opts = function(_, opts)
+			opts.presets.lsp_doc_border = true
+			table.insert(opts.routes, {
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+		},
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		opts = function(_, opts)
+			local logo = [[
+██╗  ██╗██╗███████╗██╗  ██╗
+╚██╗██╔╝██║██╔════╝██║  ██║
+ ╚███╔╝ ██║███████╗███████║
+ ██╔██╗ ██║╚════██║██╔══██║
+██╔╝ ██╗██║███████║██║  ██║
+╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝
+                           
+      ]]
+
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
+			opts.config.header = vim.split(logo, "\n")
+		end,
+	},
+}
