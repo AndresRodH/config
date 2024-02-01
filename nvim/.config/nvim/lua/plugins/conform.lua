5d406e4d45de5f6949f5b9da188ff2b2535d2656
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-02-01 08:00:30 -0600

    fix biome not being picked up as the default formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,15 +1,26 @@
 return {
-	"stevearc/conform.nvim",
-	optional = true,
-	opts = {
-		formatters_by_ft = {
-			["javascript"] = { "biome", "prettier" },
-			["javascriptreact"] = { "biome", "prettier" },
-			["typescript"] = { "biome", "prettier" },
-			["typescriptreact"] = { "biome", "prettier" },
-			["html"] = { "biome", "prettier" },
-			["json"] = { "biome", "prettier" },
-			["jsonc"] = { "biome" },
+	{
+		"stevearc/conform.nvim",
+		optional = true,
+		opts = {
+			formatters_by_ft = {
+				["javascript"] = { { "biome", "prettier" } },
+				["javascriptreact"] = { { "biome", "prettier" } },
+				["typescript"] = { { "biome", "prettier" } },
+				["typescriptreact"] = { { "biome", "prettier" } },
+				["html"] = { { "biome", "prettier" } },
+				["json"] = { { "biome", "prettier" } },
+				["jsonc"] = { "biome" },
+				["yaml"] = { "prettier" },
+				["md"] = { "prettier" },
+				["mdx"] = { "prettier" },
+			},
 		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			table.insert(opts.ensure_installed, "biome")
+		end,
 	},
 }
