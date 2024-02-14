4e856c8ed54e2a63b1b14b69b2534f83cc352d2e
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-02-14 17:44:14 -0600

    add templ to lspconfig

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,22 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      graphql = {
-        filetypes = { "graphql", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-      },
-    },
-  },
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			graphql = {
+				filetypes = { "graphql", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+			},
+			html = {
+				filetypes_include = { "templ" },
+			},
+			htmx = {
+				filetypes_include = { "templ" },
+			},
+			tailwindcss = {
+				filetypes_include = { "templ" },
+			},
+		},
+	},
+	init = function()
+		vim.filetype.add({ extension = { templ = "templ" } })
+	end,
 }
